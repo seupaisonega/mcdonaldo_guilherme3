e71350fa043c0cb012516bae28afcecbb1121499
SPACEzinxl <samuelfernandoa981725760@gmail.com>
2026-09-03 14:16:35 -0300

    u

--- app/src/main/java/com/example/mcdonaldo_guilherme/pedidos.java
+++ app/src/main/java/com/example/mcdonaldo_guilherme/pedidos.java
@@ -7,27 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.content.Intent;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class pedidos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_pedidos);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button btnEntrar = findViewById(R.id.button);
-        btnEntrar.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this,
-                    pedidos.class);
-
-            startActivity(intent);
-        });
-            }
+    }
 }
